4505f86867ec34a6a493a5e97c78a54da2308cd4
Johngeek <johngeek2017@gmail.com>
2025-08-25 14:51:31 -0400

    Reparando el menu movil

--- src/app/components/layout/bottom-nav/bottom-nav.component.ts
+++ src/app/components/layout/bottom-nav/bottom-nav.component.ts
@@ -2,15 +2,17 @@ import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UiService } from '../../../core/services/ui.service';
 import { CartStore } from '../../../features/cart/cart.store';
+// ✅ NUEVO: Se importa RouterModule para habilitar las directivas de enrutamiento.
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-bottom-nav',
   standalone: true,
-  imports: [CommonModule],
+  // ✅ CAMBIO: Se añade RouterModule al array de imports.
+  imports: [CommonModule, RouterModule],
   templateUrl: './bottom-nav.component.html',
 })
 export class BottomNavComponent {
-  // Hacemos el servicio público para poder usarlo en la plantilla HTML
   public uiService = inject(UiService);
   public cartStore = inject(CartStore);
 }
