849de0ae54a1c3081faedec004f4eccece77645e
Johngeek <johngeek2017@gmail.com>
2025-08-25 14:59:19 -0400

    Correcion visual

--- src/app/components/layout/bottom-nav/bottom-nav.component.ts
+++ src/app/components/layout/bottom-nav/bottom-nav.component.ts
@@ -2,13 +2,11 @@ import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UiService } from '../../../core/services/ui.service';
 import { CartStore } from '../../../features/cart/cart.store';
-// ✅ NUEVO: Se importa RouterModule para habilitar las directivas de enrutamiento.
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-bottom-nav',
   standalone: true,
-  // ✅ CAMBIO: Se añade RouterModule al array de imports.
   imports: [CommonModule, RouterModule],
   templateUrl: './bottom-nav.component.html',
 })
