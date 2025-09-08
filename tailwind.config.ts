65ec5e025b6261540a571f7d7c48c77137d3a4d1
Johngeek <johngeek2017@gmail.com>
2025-09-08 15:56:42 -0400

    Correcion visual

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,10 @@ const config: Config = {
     './src/**/*.html',
     './src/**/*.ts',
   ],
+  safelist: [
+    'swiper-pagination-bullet',
+    'swiper-pagination-bullet-active'
+  ],
   theme: {
     extend: {
       aspectRatio: {
